94c176c2ff04fcf17477e03ebbacebdfa64622ed
Alfandaw <115681070+Alfandaw@users.noreply.github.com>
2022-10-16 18:03:41 +0700

    Add files via upload

---
+++ OJK.php
@@ -0,0 +1,26 @@
+<?php
+$data = file_get_contents("https://ojk-invest-api.vercel.app/api/illegals");
+$data_ojkinvest=json_decode($data);
+$table = "<h3>OJK INVESTS</h3>";
+$table .= "<table border=1>
+               <tr><td>No</td>
+			    <td>Nama PT</td>
+				<td>Alamat</td>	
+				<td>Telp<td/>			
+				<td>Tipe</td>
+				<td>Web</td>
+				</tr>";
+
+for($i=0; $i<count($data_ojkinvest->data->illegals);$i++){
+	$id=$i+1;
+	$table .= "<tr><td>{$id}</td>
+			    <td>{$data_ojkinvest->data->illegals[$i]->name}</td>
+				<td>{$data_ojkinvest->data->illegals[$i]->address}</td>
+				<td>{$data_ojkinvest->data->illegals[$i]->number[0]}</td>
+				<td>{$data_ojkinvest->data->illegals[$i]->type}</td>
+				<td>{$data_ojkinvest->data->illegals[$i]->urls[0]}</td>
+				</tr>";
+}
+$table .= "</table>";
+echo $table;				
+?>
